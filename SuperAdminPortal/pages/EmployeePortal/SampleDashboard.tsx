570ca5e481e486994f0e240cf568ee42603680d2
Zahid <70480085+zahidpanhwar@users.noreply.github.com>
2025-09-20 23:38:57 +1000

    Add SampleDashboard component for layout testing

---
+++ SuperAdminPortal/pages/EmployeePortal/SampleDashboard.tsx
@@ -0,0 +1,16 @@
+// NOTE: This is a sample/demo page for testing Layout only.
+// Do not use in production.
+
+import React from "react";
+import Layout from "../../shared/Layout";
+
+const SampleDashboard: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Sample Dashboard</h1>
+      <p>This page is only for testing the new Layout skeleton.</p>
+    </Layout>
+  );
+};
+
+export default SampleDashboard;
